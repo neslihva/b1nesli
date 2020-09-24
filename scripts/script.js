e146824efa0de2791d087072e84d9cf418532f20
neslihva <70858706+neslihva@users.noreply.github.com>
2020-09-25 00:19:41 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+var burger = document.querySelector('.burger');
+var nav = document.querySelector('.nav-links');
+
+function navSlide() { nav.classList.toggle('nav-active');}
+
+burger.addEventListener('click', navSlide)
