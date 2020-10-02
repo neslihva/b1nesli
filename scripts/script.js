739d4ca807d6a535e1538268523326e245fdd109
neslihva <70858706+neslihva@users.noreply.github.com>
2020-10-02 02:08:19 +0200

    versie02

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-var burger = document.querySelector('.burger');
-var nav = document.querySelector('.nav-links');
-
-function navSlide() { nav.classList.toggle('nav-active');}
-
-burger.addEventListener('click', navSlide)
